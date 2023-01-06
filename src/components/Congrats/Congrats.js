bedc0a25d173d43de11300c2fe32c8f88f1634d0
anazks <anazksunil2@gmail.com>
2023-01-06 14:13:05 +0530

    0.8

--- src/components/Congrats/Congrats.js
+++ src/components/Congrats/Congrats.js
@@ -1,15 +1,30 @@
 import React, { useEffect,useState } from 'react'
 import { useSearchParams } from 'react-router-dom';
-import { AiFillCopy } from "react-icons/ai";
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+} from 'react-share';
 import { useApi } from "../../utils/hooks";
 import PageLayout, { SectionContainer } from '../../screens/Landing/partials/Layout'
 import { AuthenticatedHeader, Header } from '../../screens/Landing/partials'
 import './congrats.scss'
 import useUserAccount from '../../hooks/useUserAccount';
-import { USERDATA } from '../../utils/constants';
+import { BASENAME } from '../../utils/constants';
 
 
+const IS_DEV = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+const BASE_ASSETS = process.env.REACT_APP_IS_TEST_BUILD ? '.' : '';
+const referralLink = IS_DEV ? `${window.location.hostname}:3000${BASENAME}` : `cl-cards.com/waiting-list`;
+const SHARING_MESSAGE =
+  "I've just joined the waitlist of the VetCard, powered by @Sentinel Digital.";
+const HASHTAG = 'yourcryptoyourlife';
 function Congrats() {
+  const [copied, setCopied] = useState(false);
+
+  
+
   const [user] = useSearchParams();
   const [userData, setUserData] = useState('')
   const UID = user.get('uid');
@@ -17,7 +32,7 @@ function Congrats() {
   const  { data,refetch } =   useApi('post', `/v2/account/loginWithUID`, { type: 'ledger' },);
   console.log(data)
   const { authAccount,account } = useUserAccount();
-
+  
 
  
   // console.log(data);
@@ -25,18 +40,19 @@ function Congrats() {
  
   
   useEffect(() => {  
-    refetch({
-      "uid" :UID
-    })
+    
+    if(UID){
+      refetch({
+        "uid" :UID
+      })
+    }
     if(data){
        setUserData(data)
         console.log(data)
-        console.log("userData...",userData)
+        console.log(userData)
         const { email, firstName, referlink,created_at: createdAt, ecosystem_uuid:ecosystemUuid } = data;
-        USERDATA.userData = data
-        if (ecosystemUuid) {
+        if (data) {
           authAccount(email, firstName, referlink, createdAt, ecosystemUuid);
-          document.querySelector("#membermodal .close")?.click();
         }
         else {
           alert(data)
@@ -44,9 +60,15 @@ function Congrats() {
       }
   },[])
 
-  
+const refLink = `https://${referralLink}/register?ref=${account.referlink}`;
+const handleCopyClick = () => {
+  navigator.clipboard.writeText(refLink);
+  setCopied(true);
+};
+
 return (
   <PageLayout>
+     
        {(!account.ecosystem_uuid) &&
         <Header />
       }
@@ -72,17 +94,56 @@ return (
                   Get reward when they join with your unique referel link
                 </p>
                 <p className='smallText'>Move up the waitinglist when others join uisng your referral link</p>
-                <div className='clipBox'>
-                  <p className='clipText'>https://localhost/waiting-list/register?ref=ob1Ttsyl<AiFillCopy className='icons' /> </p>
-                </div>
-                <div className="social">
+                <div className='clipBox' style={{display:"flex"}}>
 
-                  <img src="/assets/images/twitter-ref.svg" style={{backgroundColor:"rgb(25, 167, 223)"}} alt="" />
-                  <img src="/assets/images/linkedin-ref.svg" style={{backgroundColor:"rgb(42, 58, 184)"}} alt="" />
-                  <img src="/assets/images/facebook.svg" style={{backgroundColor:"rgb(19, 8, 243)"}}  alt="" />
-                  <img src="/assets/images/whatsapp-ref.svg" style={{backgroundColor:"green"}}  alt="" />
-
+                  <p className='clipText'>https://localhost/waiting-list/register?ref=ob1Ttsyl</p>
+                  <div onClick={handleCopyClick} aria-hidden="true" className="copy float-end m-0 clipText">
+                    <img
+                      src={`${BASE_ASSETS}/assets/images/${copied ? 'check-icon' : 'copy'}.svg`}
+                      alt="copy" className='copyImg' style={{width:"20px",height:"auto"}}
+                    />
+                  </div>
                 </div>
+                <div className="d-flex flex-row social-media-container social" >
+              <TwitterShareButton url={refLink} title={SHARING_MESSAGE} hashtags={[HASHTAG]}>
+                <div className="twitter-background social-media-link ms-0 m-2">
+                  <img
+                    src={`${BASE_ASSETS}/assets/images/twitter-ref.svg`}
+                    alt="Twitter"
+                    className="social-media-icon"
+                  />
+                </div>
+              </TwitterShareButton>
+              <LinkedinShareButton url={refLink} title={`${SHARING_MESSAGE}\n\n${HASHTAG}`}>
+                <div className="linkedin-background social-media-link m-2">
+                  <img
+                    src={`${BASE_ASSETS}/assets/images/linkedin-ref.svg`}
+                    alt="Linkedin"
+                    className="social-media-icon"
+                  />
+                </div>
+              </LinkedinShareButton>
+              <FacebookShareButton url={refLink} quote={SHARING_MESSAGE} hashtag={HASHTAG}>
+                <div className="facebook-background social-media-link m-2">
+                  <img
+                    src={`${BASE_ASSETS}/assets/images/facebook-ref.svg`}
+                    alt="Facebook"
+                    className="social-media-icon"
+                  />
+                </div>
+              </FacebookShareButton>
+
+              <WhatsappShareButton url={refLink} title={SHARING_MESSAGE} separator={'\n'}>
+                <div className="whatsapp-background social-media-link m-2">
+                  <img
+                    src={`${BASE_ASSETS}/assets/images/whatsapp-ref.svg`}
+
+                    alt="Whatsapp"
+                    className="social-media-icon"
+                  />
+                </div>
+              </WhatsappShareButton>
+            </div>
               </div>
 
             </div>
